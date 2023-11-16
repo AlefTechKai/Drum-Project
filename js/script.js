9d9bd1747426d121d49ad3a3fa281a2e320c57ba
Alef B <104469492+AlefTechKai@users.noreply.github.com>
2023-11-15 23:36:48 -0300

    Update script.js
    correct src

--- js/script.js
+++ js/script.js
@@ -19,37 +19,37 @@ function makeSound(key) {
     switch (key) {
       case "A":
       case "a":
-        var leftCrash = new Audio("../assets/sounds/leftCrash.mp3");
+        var leftCrash = new Audio("assets/sounds/leftCrash.mp3");
         leftCrash.play();
         break;
       case "S":
       case "s":
-        var tom1 = new Audio("../assets/sounds/tom1.mp3");
+        var tom1 = new Audio("assets/sounds/tom1.mp3");
         tom1.play();
         break;
       case "D":
       case "d":
-        var snare = new Audio("../assets/sounds/snare.mp3");
+        var snare = new Audio("assets/sounds/snare.mp3");
         snare.play();
         break;
       case "G":
       case "g":
-        var kickbass = new Audio("../assets/sounds/kickbass.mp3");
+        var kickbass = new Audio("assets/sounds/kickbass.mp3");
         kickbass.play();
         break;
       case "J":
       case "j":
-        var tom3 = new Audio("../assets/sounds/tom3.mp3");
+        var tom3 = new Audio("assets/sounds/tom3.mp3");
         tom3.play();
         break;
       case "K":
       case "k":
-        var tom2 = new Audio("../assets/sounds/tom2.mp3");
+        var tom2 = new Audio("assets/sounds/tom2.mp3");
         tom2.play();
         break;
       case "L":
       case "l":
-        var rightCrash = new Audio("../assets/sounds/rightCrash.mp3");
+        var rightCrash = new Audio("assets/sounds/rightCrash.mp3");
         rightCrash.play();
         break;
     }
